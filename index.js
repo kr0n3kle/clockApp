89746a15839c5fa7caac246ee3e5e522f3013c3c
Kr0n3kLe <jasonkeefauver@icloud.com>
2024-10-11 19:19:52 -0600

    js file upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// 
+
+function updateClock() {
+
+    const now = new Data();
+    const hours = now.getHours();
+    const mintues = now.getMintues();
+    const seconds = now.getSeconds();
+}
+
+updateClock();
