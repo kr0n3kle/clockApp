84f058c795c62508d8d613e8cbaf2f3632b879bc
Kr0n3kLe <jasonkeefauver@icloud.com>
2024-10-15 21:11:12 -0600

    Initial functional JS

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 
 function updateClock() {
 
-    const now = new Data();
-    const hours = now.getHours();
-    const mintues = now.getMintues();
-    const seconds = now.getSeconds();
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString();
+    const seconds = now.getSeconds().toString();
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = timeString;
 }
 
-updateClock();
+updateClock(); 
+setInterval();
